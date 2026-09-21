8998211fb63225870dd8462c0a68bc9c917ab5ca
sujeth95 <sujeetoraon337@gmail.com>
2026-09-21 11:18:36 +0530

    Schema Created

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,24 @@
 import express from 'express';
 import mongoose from 'mongoose';
+import jwt from 'jsonwebtoken';
+import { UserModel } from './db.js';
+
 const app = express();
 
 
-app.post("/api/v1/signin", (req, res) => {
+app.post("/api/v1/signin", async (req, res) => {
+    // zod validation, hash the password
+    const username = req.body.username;
+    const password = req.body.password;
 
+    await UserModel.create({
+        username: username,
+        password: password
+    })
+
+    res.json({
+        message: "User signed up"
+    })
 })
 
 app.post("/api/v1/content", (req, res) => {
@@ -24,5 +38,5 @@ app.post("/api/v1/brain/share", (req, res) => {
 })
 
 app.get("/api/v1/brain/:shareLink", (req,res) => {
-    
+
 })
